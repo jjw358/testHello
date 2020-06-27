ca34753003a1cc30229a3da6178ada6f52fca526
jjw358 <jjw358@gmail.com>
2020-06-27 16:06:44 -0500

    third commit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='helloworld',
+    name='helloworld_jw',
     version='0.0.1',
     description='say hello!',
     py_modules=['helloworld', 'f2'],
